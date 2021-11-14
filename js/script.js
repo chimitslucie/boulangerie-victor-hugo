8cbc0effe92b34c08bde5b01bba73fc0f17b960c
lucie chimits <chimitslucie@gmail.com>
2021-11-14 18:54:56 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#slider-header').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        fadeSpeed: 1000,
+    });
+
+    $('#slider-promo').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        fadeSpeed: 1000,
+    });
+
+    $('#slider-team').slick({
+        infinite: true,
+        dots: false,
+        arrows: true,
+        autoplay: false,
+    });
+});
